baa2151c0a410e411fe64f58a705100e48da4097
姚观寿 <281113270@qq.com>
2021-10-31 21:12:33 +0800

    哈希表

--- javascript/字典/01.js
+++ javascript/字典/01.js
@@ -7,37 +7,53 @@
  * @FilePath: /algorithm/javascript/字典/01.js
  */
 function Dictionary() {
-  this.dataStore = new Array();
+    this.dataStore = new Array();
 }
 
 Dictionary.prototype = {
-  add(key, value) {
-    this.dataStore[key] = value;
-  },
-  find(key) {
-    return this.dataStore[key];
-  },
-  remove(key) {
-    delete this.dataStore[key];
-  },
-  map(callback = () => {}) {
-    for (let key of Object.keys(this.dataStore)) {
-      callback(this.dataStore[key], key);
-    }
-  },
+    set(key, value) {
+        this.dataStore[key] = value;
+    },
+
+    get(key) {
+        return this.dataStore[key];
+    },
+    delete(key) {
+        delete this.dataStore[key];
+    },
+    has(key) { // 判断给定的键值是否存在于字典中
+        return this.dataStore.hasOwnProperty(key);
+    },
+    clear() { // 清空字典内容
+        this.dataStore = {};
+    },
+    keys() { // 返回字典中所有的键值
+        return Object.keys(this.dataStore);
+    },
+    values() { // 返回字典中所有的值
+        return Object.values(this.items);
+    },
+    getItems() { // 返回字典中的所有元素
+        return this.items;
+    },
+    map(callback = () => {}) {
+        for (let key of Object.keys(this.dataStore)) {
+            callback(this.dataStore[key], key);
+        }
+    },
 };
 
 let d = new Dictionary();
-d.add("a", 1);
-d.add("b", 2);
-d.add("c", 3);
-d.add("d", 4);
-d.add("e", 5);
-d.add("f", 6);
-d.add("g", 7);
-d.map((item,key)=>{
-    console.log('item=======',item)
-    console.log('key=======',key)
+d.set("a", 1);
+d.set("b", 2);
+d.set("c", 3);
+d.set("d", 4);
+d.set("e", 5);
+d.set("f", 6);
+d.set("g", 7);
+d.map((item, key) => {
+    console.log('item=======', item)
+    console.log('key=======', key)
 })
 
 // console.log("d======", Object.keys(d.dataStore));
